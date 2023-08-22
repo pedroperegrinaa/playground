7c695bee6858dea4c67e7f21ce935ddbc3cc15d5
Pedro Peregrina <pedrosilva1p333@gmail.com>
2023-08-22 01:08:09 -0300

    feat: ending 1 part of freelas-faculdade

--- freelas-faculdade/cars.py
+++ freelas-faculdade/cars.py
@@ -1,5 +1,12 @@
 import datetime
 
+# Sequências de escape ANSI para cores
+RESET = "\033[0m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+CYAN = "\033[36m"
+RED = "\033[31m"
+
 def calcular_valor_estacionamento(entrada, saida):
     valor_hora = 5  # Valor cobrado por hora
     periodo = saida - entrada  # Calcula o período de tempo em que o veículo ficou estacionado
@@ -9,14 +16,29 @@ def calcular_valor_estacionamento(entrada, saida):
 
     return valor_cobrado
 
-# Solicitação de entrada de dados interativa
-print("Bem-vindo ao sistema de cálculo de valor de estacionamento.")
-entrada_str = input("Digite a data e hora de entrada (formato: YYYY-MM-DD HH:MM): ")
-saida_str = input("Digite a data e hora de saída (formato: YYYY-MM-DD HH:MM): ")
+def main():
+    print(GREEN + "Bem-vindo ao sistema de cálculo de valor de estacionamento. \n")
+    print(YELLOW + "Digite 0 e Enter para encerrar do programa.")
 
-# Converter strings de entrada e saída para objetos datetime
-entrada = datetime.datetime.strptime(entrada_str, "%Y-%m-%d %H:%M")
-saida = datetime.datetime.strptime(saida_str, "%Y-%m-%d %H:%M")
+    while True:
+        entrada_str = input(CYAN + "Digite a data e hora de entrada (formato: YYYY-MM-DD HH:MM): ")
+        if entrada_str == "0":
+            print(RED + "Encerrando o programa...")
+            break
+        saida_str = input(CYAN + "Digite a data e hora de saída (formato: YYYY-MM-DD HH:MM): ")
+        if saida_str == "0":
+            print(RED + "Encerrando o programa...")
+            break
 
-valor_total = calcular_valor_estacionamento(entrada, saida)
-print(f"O valor total a ser cobrado é R${valor_total:.2f}")
+        try:
+            # Converter strings de entrada e saída para objetos datetime
+            entrada = datetime.datetime.strptime(entrada_str, "%Y-%m-%d %H:%M")
+            saida = datetime.datetime.strptime(saida_str, "%Y-%m-%d %H:%M")
+
+            valor_total = calcular_valor_estacionamento(entrada, saida)
+            print(f"O valor total a ser cobrado é R${valor_total:.2f}")
+        except ValueError:
+            print(RED + "Formato inválido. Certifique-se de inserir as datas no formato correto.")
+
+if __name__ == "__main__":
+    main()
--- freelas-faculdade/money.py
+++ freelas-faculdade/money.py
@@ -1,6 +1,13 @@
 import json
 import urllib.request
 
+# Sequências de escape ANSI para cores
+RESET = "\033[0m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+CYAN = "\033[36m"
+RED = "\033[31m"
+
 def obter_cotacao_dolar_para_rupia():
     url = "https://open.er-api.com/v6/latest/USD"
     
@@ -12,7 +19,7 @@ def obter_cotacao_dolar_para_rupia():
             cotacao = data["rates"]["INR"]
             return cotacao
         else:
-            print("Não foi possível obter a cotação. Verifique a API ou tente novamente mais tarde.")
+            print(RED + "Não foi possível obter a cotação. Verifique a API ou tente novamente mais tarde.")
             return None
     except Exception as e:
         print(f"Erro ao obter cotação: {e}")
@@ -22,12 +29,20 @@ def converter_dolar_para_rupia(dolar, taxa):
     rupia = dolar * taxa
     return rupia
 
-try:
-    cotacao_dolar_para_rupia = obter_cotacao_dolar_para_rupia()
+while True:
+    print(YELLOW + "Digite 0 e Enter para encerrar do programa.")
+    try:
+        cotacao_dolar_para_rupia = obter_cotacao_dolar_para_rupia()
 
-    if cotacao_dolar_para_rupia is not None:
-        valor_em_dolar = float(input("Digite o valor em DÓLAR AMERICANO: "))
-        valor_em_rupia = converter_dolar_para_rupia(valor_em_dolar, cotacao_dolar_para_rupia)
-        print(f"O valor em Rúpia Indiana é: {valor_em_rupia:.2f} INR")
-except ValueError:
-    print("Por favor, insira um valor numérico válido.")
+        if cotacao_dolar_para_rupia is not None:
+            valor_em_dolar_str = input(CYAN + "Digite o valor em DÓLAR AMERICANO (ou 0 para encerrar): ")
+            
+            if valor_em_dolar_str == "0":
+                print(RED + "Encerrando o programa...")
+                break
+                
+            valor_em_dolar = float(valor_em_dolar_str)
+            valor_em_rupia = converter_dolar_para_rupia(valor_em_dolar, cotacao_dolar_para_rupia)
+            print(f"O valor em Rúpia Indiana é: {valor_em_rupia:.2f} INR")
+    except ValueError:
+        print(RED + "Por favor, insira um valor numérico válido.")
